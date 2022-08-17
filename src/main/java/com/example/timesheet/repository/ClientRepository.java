18a8bb541bedd0c3356d406a63819667b59d651d
Stefan Vlajkovic <stefan.vlajkovic@vega.local>
2022-08-17 15:21:40 +0200

    Update validation

--- src/main/java/com/example/timesheet/repository/ClientRepository.java
+++ src/main/java/com/example/timesheet/repository/ClientRepository.java
@@ -1,12 +1,11 @@
 package com.example.timesheet.repository;
 
 import com.example.timesheet.model.entity.Client;
-import com.example.timesheet.model.entity.Country;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface ClientRepository extends JpaRepository<Client, Integer> {
 
-    boolean existsClientByNameAndAddressAndCityAndZipAndCountry(String name, String address, String city, Integer zip, Country country);
+    boolean existsClientByName(String name);
 }
