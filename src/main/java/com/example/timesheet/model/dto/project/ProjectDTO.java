3f57b475c9a5c30bbb9ba7658a65a754d5f28569
VEGA\stefan.vlajkovic <VEGA\stefan.vlajkovic>
2022-08-19 09:11:40 +0200

    Update validation for ProjectDTO

--- src/main/java/com/example/timesheet/model/dto/project/ProjectDTO.java
+++ src/main/java/com/example/timesheet/model/dto/project/ProjectDTO.java
@@ -3,13 +3,22 @@ package com.example.timesheet.model.dto.project;
 import com.example.timesheet.model.dto.employee.EmployeeDTO;
 import com.example.timesheet.model.enumeration.ProjectStatus;
 import lombok.Data;
+import org.hibernate.validator.constraints.Length;
+
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
 
 @Data
 public class ProjectDTO {
 
     private Integer id;
+    @NotBlank(message = "Name is mandatory")
+    @Length(min = 3, max = 30, message = "Name must be between 3 and 30 characters.")
     private String name;
+    @NotBlank(message = "Description is mandatory")
+    @Length(min = 3, max = 30, message = "Description must be between 3 and 30 characters.")
     private String description;
+    @NotNull(message = "Status is mandatory")
     private ProjectStatus status;
     private EmployeeDTO lead;
 }
